6ffffeb0641b3bfdb9ddc2ce9a44ae7727f10b3f
Vanguards <1094194524@qq.com>
2019-10-17 16:35:49 +0800

    Initial commit

---
+++ bayes.py
@@ -0,0 +1,61 @@
+from numpy import *
+
+
+
+
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]    #1代表侮辱性文字，0代表正常言论
+    return postingList,classVec
+
+
+def createVocabList(dataSet):
+    vocabSet=set([])
+    for document in dataSet:
+        vocabSet=vocabSet|set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList,inputSet):
+    returnVec=[0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)]=1
+        else:print("the word: %s is not in my Vocabulary!"%word)
+    return returnVec
+
+def trainNB(trainMatrix,trainCategory):
+    numTrainDocs=len(trainMatrix)
+    numWords=len(trainMatrix[0])
+    pAbusive=sum(trainCategory)/float(numTrainDocs)
+    p0Num=zeros(numWords)
+    p1Num=zeros(numWords)
+    p0Denom=0.0
+    p1Denom=0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i]==1:
+            p1Num+=trainMatrix[i]
+            p1Denom+=sum(trainMatrix[i])
+            print(p1Num)
+            print(p1Denom)
+        else:
+            p0Num+=trainMatrix[i]
+            p0Denom+=sum(trainMatrix[i])
+    p1Vect=p1Num/p1Denom
+    p0Vect=p0Num/p0Denom
+    return p0Vect,p1Vect,pAbusive
+
+
+listOPosts,listClasses=loadDataSet()
+myVocabList=createVocabList(listOPosts)
+trainMat=[]
+for postinDoc in listOPosts:
+    trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+print(trainMat)
+p0V,p1V,pAb=trainNB(trainMat,listClasses)
+print(p1V)
+
